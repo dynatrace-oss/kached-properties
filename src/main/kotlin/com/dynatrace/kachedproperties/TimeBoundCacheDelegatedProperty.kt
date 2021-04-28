490c79275cc2634b4d9689be7a4622d92e34ae85
Uriel Salischiker <uriel@urielsalis.me>
2021-04-28 12:08:08 +0200

    Refactor kotlin version
    Cleans up the code, adds a way to control if we want to update the value in the background or immediately, adds a way to set a custom executor for using a specific thread pool

--- src/main/kotlin/com/dynatrace/kachedproperties/TimeBoundCacheDelegatedProperty.kt
+++ src/main/kotlin/com/dynatrace/kachedproperties/TimeBoundCacheDelegatedProperty.kt
@@ -1,52 +1,74 @@
 package com.dynatrace.kachedproperties
 
-import kotlin.concurrent.thread
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
 import kotlin.reflect.KProperty
 import kotlin.time.Duration
 import kotlin.time.ExperimentalTime
 import kotlin.time.TimeSource
 
 @ExperimentalTime
-fun <T : Any> cachedFor(updateDuration: Duration, provideValue: () -> T) =
-    TimeBoundCacheDelegatedProperty(updateDuration, provideValue, TimeSource.Monotonic)
+fun <T : Any> cachedWithLazyRefreshFor(
+    updateDuration: Duration,
+    provideValue: () -> T,
+    executor: Executor = Executors.newCachedThreadPool(),
+    timeSource: TimeSource = TimeSource.Monotonic
+) =
+    TimeBoundCacheDelegatedProperty(updateDuration, provideValue, true, executor, timeSource)
 
 @ExperimentalTime
-// Used in tests.
-internal fun <T : Any> cachedFor(updateDuration: Duration, provideValue: () -> T, timeSource: TimeSource) =
-    TimeBoundCacheDelegatedProperty(updateDuration, provideValue, timeSource)
+fun <T : Any> cachedFor(
+    updateDuration: Duration,
+    provideValue: () -> T,
+    timeSource: TimeSource = TimeSource.Monotonic
+) =
+    TimeBoundCacheDelegatedProperty(updateDuration, provideValue, false, null, timeSource)
 
 @ExperimentalTime
 class TimeBoundCacheDelegatedProperty<T : Any>(
     private val updateDuration: Duration,
     private val provideValue: () -> T,
+    private val lazyRefresh: Boolean,
+    private val executor: Executor?,
     private val timeSource: TimeSource,
 ) {
-    // The initial value is set this way to ensure that the first request to getValue will cause fetching the value.
-    private var lastFetchedTimeMark = timeSource.markNow().minus(updateDuration * 2)
-    private var firstInit = false
+    private var lastFetchedTimeMark = timeSource.markNow()
+    private var notFirstInit = false
     private var beingUpdated = false
     private lateinit var lastValue: T
 
     operator fun getValue(thisRef: Any?, property: KProperty<*>): T {
-        synchronized(firstInit) {
-            if (firstInit) {
-                if (lastFetchedTimeMark.elapsedNow() >= updateDuration) {
-                    val last = lastValue
-                    if (!beingUpdated) {
-                        beingUpdated = true
-                        thread(start = true) {
-                            updateValue()
-                            beingUpdated = false
-                        }
-                    }
-                    return last
-                }
-            } else {
+        synchronized(notFirstInit) {
+            if (!notFirstInit) {
                 updateValue()
-                firstInit = true
+                notFirstInit = true
+            }
+
+            if (lastFetchedTimeMark.elapsedNow() >= updateDuration) {
+                return queueUpdateValue()
             }
         }
         return lastValue
+    }
+
+    private fun queueUpdateValue(): T {
+        val last = lastValue
+        if (beingUpdated) {
+            return last
+        }
+
+        beingUpdated = true
+        if (!lazyRefresh || executor == null) {
+            updateValue()
+            beingUpdated = false
+            return lastValue
+        }
+
+        executor.execute {
+            updateValue()
+            beingUpdated = false
+        }
+        return last
     }
 
     private fun updateValue() {
--- src/test/kotlin/com/dynatrace/kachedproperties/TimeBoundCacheDelegatedPropertyTest.kt
+++ src/test/kotlin/com/dynatrace/kachedproperties/TimeBoundCacheDelegatedPropertyTest.kt
@@ -6,6 +6,8 @@ import io.mockk.verify
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import java.lang.Thread.sleep
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
 import java.util.concurrent.atomic.AtomicInteger
 import kotlin.concurrent.thread
 import kotlin.time.ExperimentalTime
@@ -15,11 +17,11 @@ import kotlin.time.seconds
 @ExperimentalTime
 class TimeBoundCacheDelegatedPropertyTest {
     @Test
-    fun `simple case - two subsequent value requests`() {
+    fun `should return the same value when cache didn't expire`() {
         val mockTimeSource = TestTimeSource()
         val fetchValueFromDependency = mockk<() -> String>()
         every { fetchValueFromDependency() } returnsMany listOf("Call 1", "Call 2")
-        val cachedValue by cachedFor(5.seconds, { fetchValueFromDependency() }, mockTimeSource)
+        val cachedValue by cachedWithLazyRefreshFor(5.seconds, { fetchValueFromDependency() }, timeSource = mockTimeSource)
 
         assertEquals("Call 1", cachedValue)
         mockTimeSource += 2.seconds
@@ -29,7 +31,7 @@ class TimeBoundCacheDelegatedPropertyTest {
     }
 
     @Test
-    fun `value request after cached value expiration`() {
+    fun `should update the value after the cache expires`() {
         val mockTimeSource = TestTimeSource()
         val fetchValueFromDependency = mockk<() -> String>()
         every { fetchValueFromDependency() } returnsMany listOf("Call 1", "Call 2")
@@ -37,15 +39,41 @@ class TimeBoundCacheDelegatedPropertyTest {
 
         assertEquals("Call 1", cachedValue)
         mockTimeSource += 10.seconds
-        assertEquals("Call 1", cachedValue)
-        sleep(100)
         assertEquals("Call 2", cachedValue)
 
         verify(exactly = 2) { fetchValueFromDependency() }
     }
 
     @Test
-    fun `second value request during fetching value after first value request`() {
+    fun `should return same value when different threads call it for updating with lazy refresh `() {
+        val mockTimeSource = TestTimeSource()
+        val fetchValueFromDependency = mockk<() -> String>()
+        val dependencyCallCounter = AtomicInteger(1)
+        every { fetchValueFromDependency() } answers {
+            sleep(1.seconds.toLongMilliseconds()) // Simulating long network call.
+            "Call ${dependencyCallCounter.getAndIncrement()}"
+        }
+        val cachedValue by cachedWithLazyRefreshFor(5.seconds, { fetchValueFromDependency() }, timeSource = mockTimeSource)
+
+        lateinit var cachedValueReadFromThread1: String
+        lateinit var cachedValueReadFromThread2: String
+        val thread1 = thread {
+            cachedValueReadFromThread1 = cachedValue
+        }
+        val thread2 = thread {
+            cachedValueReadFromThread2 = cachedValue
+        }
+        thread1.join()
+        thread2.join()
+
+        val retrievedValues = setOf(cachedValueReadFromThread1, cachedValueReadFromThread2)
+        assertEquals(setOf("Call 1"), retrievedValues)
+
+        verify(exactly = 1) { fetchValueFromDependency() }
+    }
+
+    @Test
+    fun `should return same value when different threads call it for updating without lazy refresh`() {
         val mockTimeSource = TestTimeSource()
         val fetchValueFromDependency = mockk<() -> String>()
         val dependencyCallCounter = AtomicInteger(1)
@@ -70,5 +98,49 @@ class TimeBoundCacheDelegatedPropertyTest {
         assertEquals(setOf("Call 1"), retrievedValues)
 
         verify(exactly = 1) { fetchValueFromDependency() }
+    }
+
+    @Test
+    fun `should return same value with lazy refresh`() {
+        val mockTimeSource = TestTimeSource()
+        val executor = Executors.newSingleThreadExecutor()
+        val fetchValueFromDependency = mockk<() -> String>()
+        val dependencyCallCounter = AtomicInteger(1)
+        every { fetchValueFromDependency() } answers {
+            sleep(1.seconds.toLongMilliseconds()) // Simulating long network call.
+            "Call ${dependencyCallCounter.getAndIncrement()}"
+        }
+        val cachedValue by cachedWithLazyRefreshFor(5.seconds, { fetchValueFromDependency() }, executor, mockTimeSource)
+
+        val cachedValueReadFromThread1: String = cachedValue
+        mockTimeSource += 10.seconds
+        val cachedValueReadFromThread2: String = cachedValue
+
+        val retrievedValues = setOf(cachedValueReadFromThread1, cachedValueReadFromThread2)
+        assertEquals(setOf("Call 1"), retrievedValues)
+
+        executor.awaitTermination(1, TimeUnit.SECONDS)
+        verify(exactly = 2) { fetchValueFromDependency() } // Should be updating in the background
+    }
+
+    @Test
+    fun `should return updated value immediately without lazy refresh`() {
+        val mockTimeSource = TestTimeSource()
+        val fetchValueFromDependency = mockk<() -> String>()
+        val dependencyCallCounter = AtomicInteger(1)
+        every { fetchValueFromDependency() } answers {
+            sleep(1.seconds.toLongMilliseconds()) // Simulating long network call.
+            "Call ${dependencyCallCounter.getAndIncrement()}"
+        }
+        val cachedValue by cachedFor(5.seconds, { fetchValueFromDependency() }, mockTimeSource)
+
+        val read1: String = cachedValue
+        mockTimeSource += 10.seconds
+        val read2: String = cachedValue
+
+        val retrievedValues = setOf(read1, read2)
+        assertEquals(setOf("Call 1", "Call 2"), retrievedValues)
+
+        verify(exactly = 2) { fetchValueFromDependency() }
     }
 }
