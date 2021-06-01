995099f5c593d72daa8c8c26d03b8b139dc74878
Piotr Krzeminski <piotr.krzeminski@dynatrace.com>
2021-06-01 12:41:12 +0200

    Set up publishing to Maven Central, set version to 1.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,15 @@ plugins {
 
     // Tests and code quality.
     id("io.gitlab.arturbosch.detekt") version "1.17.1"
+
+    // Publishing.
+    maven
+    `maven-publish`
+    signing
 }
 
-group = "com.dynatrace.kached-properties"
-version = "0.1.0-SNAPSHOT"
+group = "com.dynatrace"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
@@ -35,5 +40,69 @@ detekt {
     config = files("$projectDir/detekt-custom-config.yml")
     reports {
         html.enabled = false // Disabled due to requirement for kotlinx-html which is still in JCenter.
+    }
+}
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            artifactId = "kached-properties"
+            from(components["java"])
+
+            pom {
+                name.set("kached-properties")
+                description.set("Caching in Kotlin made simple.")
+                url.set("https://github.com/dynatrace-oss/kached-properties")
+
+                licenses {
+                    license {
+                        name.set("Apache License, version 2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:git://github.com/dynatrace-oss/kached-properties.git/")
+                    developerConnection.set("scm:git:ssh://github.com:dynatrace-oss/kached-properties.git")
+                    url.set("https://github.com/dynatrace-oss/kached-properties.git")
+                }
+
+                developers {
+                    developer {
+                        id.set("krzema12")
+                        name.set("Piotr Krzemiński")
+                        email.set("piotr.krzeminski@dynatrace.com")
+                    }
+                    developer {
+                        id.set("urielsalis")
+                        name.set("Uriel Salischiker")
+                        email.set("uriel.salischiker@dynatrace.com")
+                    }
+                }
+            }
+        }
+    }
+
+    val ossrhUsername: String? by project
+    val ossrhPassword: String? by project
+
+    repositories {
+        maven(url = "https://oss.sonatype.org/service/local/staging/deploy/maven2/") {
+            credentials {
+                username = ossrhUsername
+                password = ossrhPassword
+            }
+        }
+    }
+}
+
+tasks {
+    signing {
+        sign(publishing.publications["mavenJava"])
     }
 }
